f77128588df8f062587eebd68c3a19194bb4ec14
maciejmoscicki17 <73691737+maciejmoscicki17@users.noreply.github.com>
2023-03-24 21:43:24 +0100

    cars pt1, card experiments

---
+++ src/app/car-models.ts
@@ -0,0 +1,138 @@
+export const CAR_MODELS = [
+  {
+    brand: "Seat",
+    models: ["Leon ST"],
+  },
+  {
+    brand: "Cupra",
+    models: ["Formentor", "Born"],
+  },
+  {
+    brand: "Renault",
+    models: ["Mégane RS"],
+  },
+  {
+    brand: "Alfa Romeo",
+    models: ["4C", "8C", "Giulia", "Stelvio"],
+  },
+  {
+    brand: "McLaren",
+    models: ["GT", "720s"],
+  },
+  {
+    brand: "Chevrolet",
+    models: ["Camaro", "Corvette", "Suburban"],
+  },
+  {
+    brand: "GMC",
+    models: ["Hummer EV", "Yukon Denali"],
+  },
+  {
+    brand: "Hyperion",
+    models: ["XP-1"],
+  },
+  {
+    brand: "Porsche",
+    models: ["718", "911", "918", "Macan", "Cayenne", "Panamera", "Taycan"],
+  },
+  {
+    brand: "Honda",
+    models: ["Civic", "Integra", "Prelude", "NSX"],
+  },
+  {
+    brand: "Subaru",
+    models: ["BRZ", "Impreza", "WRX"],
+  },
+  {
+    brand: "Mazda",
+    models: ["MX-5", "RX-7", "RX-8"],
+  },
+  {
+    brand: "Mitsubishi",
+    models: ["Eclipse", "Lancer"],
+  },
+  {
+    brand: "Lexus",
+    models: ["LFA", "LC", "LS", "RC"],
+  },
+  {
+    brand: "Toyota",
+    models: ["Celica", "Corolla", "GT86", "MR2", "Supra", "GR Yaris"],
+  },
+  {
+    brand: "BMW",
+    models: ["i8", "M3", "M4", "M5", "M6", "M8", "Z3", "Z4", "XM"],
+  },
+  {
+    brand: "Volkswagen",
+    models: ["Golf GTI", "Phaeton", "Touareg"],
+  },
+  {
+    brand: "Mercedes-Benz",
+    models: ["190 E", "AMG GT", "S", "SLR", "Maybach Klasa V", "Maybach GLS"],
+  },
+  {
+    brand: "Audi",
+    models: ["R8", "RSQ8", "RS6", "RS7"],
+  },
+  {
+    brand: "Kia",
+    models: ["Stinger GT"],
+  },
+  {
+    brand: "Lotus",
+    models: ["Emira"],
+  },
+  {
+    brand: "Bugatti",
+    models: ["Chiron Pur Sport"],
+  },
+  {
+    brand: "Koenigsegg",
+    models: ["Regera", "Gemera"],
+  },
+  {
+    brand: "Lamborghini",
+    models: ["Aventador", "Huracan"],
+  },
+  {
+    brand: "Bentley",
+    models: ["Continental GT"],
+  },
+  {
+    brand: "Rolls-Royce",
+    models: ["Wraith"],
+  },
+  {
+    brand: "Rimac",
+    models: ["Nevera"],
+  },
+  {
+    brand: "Dodge",
+    models: ["Challenger", "Charger", "Viper"],
+  },
+  {
+    brand: "Ram Trucks",
+    models: ["1500 RTX"],
+  },
+  {
+    brand: "Ford",
+    models: ["Focus RS", "GT", "Mustang", "Raptor"],
+  },
+  {
+    brand: "Hyundai",
+    models: ["i30 N"],
+  },
+  {
+    brand: "Jaguar",
+    models: ["F-Type"],
+  },
+  {
+    brand: "Nissan",
+    models: ["200 SX", "350 Z", "370 Z", "Skyline", "GT-R"],
+  },
+  {
+    brand: "Ferrari",
+    models: ["F40", "SF90 Stradale", "Purosangue"],
+  },
+];
--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -2,6 +2,20 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { HttpClient } from "@angular/common/http";
 import { createApi } from "unsplash-js";
+import { CAR_MODELS } from "../car-models";
+import * as saveAs from "file-saver";
+
+export interface CarModel {
+  brand: string;
+  model: string;
+  fuelType: string;
+  horsePower: number;
+  transmission: string;
+  cubicCapacity: number;
+  details: string;
+  imageSrc: string;
+  price: number;
+}
 
 interface Make {
   make: string;
@@ -33,6 +47,7 @@ export class ResultsComponent implements OnInit {
   results: Car[] | null = null;
   res: Array<Car> = [];
   isLoading: boolean = false;
+  carModels = CAR_MODELS;
 
   constructor(private route: ActivatedRoute, private http: HttpClient) {
     this.route.paramMap.subscribe((params) => {
@@ -46,42 +61,74 @@ export class ResultsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.searchForCarPhoto();
+    // this.searchForCarPhoto();
+    //this.generateCars();
+  }
+
+  generateCars() {
+    const carModels: CarModel[] = [];
+
+    for (const car of CAR_MODELS) {
+      for (const model of car.models) {
+        const carModel: CarModel = {
+          brand: car.brand,
+          model: model,
+          fuelType: "",
+          horsePower: 0,
+          transmission: "",
+          cubicCapacity: 0,
+          details: "",
+          imageSrc: "",
+          price: 0,
+        };
+
+        carModels.push(carModel);
+      }
+    }
+
+    const output = { carModels };
+    const blob = new Blob([JSON.stringify(output)], {
+      type: "application/json",
+    });
+    const file = new File([blob], "cars.json", {
+      type: "application/json",
+    });
+    saveAs(file);
   }
 
   carMake = "Dodge";
   carModel = "Dakota";
   carYear = "1999";
   imageUrl: string = "";
-  searchForCarPhoto(): void {
-    this.isLoading = true;
-    unsplash.search
-      .getPhotos({
-        query: "toyota supra",
-        page: 1,
-        perPage: 1,
-        orderBy: "relevant",
-      })
-      .then((result) => {
-        if (result.errors) {
-          console.error(result.errors[0]);
-          this.isLoading = false;
-        } else {
-          unsplash.photos
-            .get({ photoId: result.response.results[0].id })
-            .then((result) => {
-              this.isLoading = false;
-              if (result.errors) {
-                console.error(result.errors[0]);
-              } else {
-                const photo = result.response;
-                this.imageUrl = photo.urls.full;
-                console.log(photo);
-              }
-            });
-        }
-      });
-  }
+  // searchForCarPhoto(): void {
+  //   this.isLoading = true;
+  //   unsplash.search
+  //     .getPhotos({
+  //       query: "toyota supra",
+  //       page: 1,
+  //       perPage: 1,
+  //       orderBy: "relevant",
+  //     })
+  //     .then((result) => {
+  //       if (result.errors) {
+  //         console.error(result.errors[0]);
+  //         this.isLoading = false;
+  //       } else {
+  //         unsplash.photos
+  //           .get({ photoId: result.response.results[0].id })
+  //           .then((result) => {
+  //             this.isLoading = false;
+  //             if (result.errors) {
+  //               console.error(result.errors[0]);
+  //             } else {
+  //               const photo = result.response;
+  //               this.imageUrl = photo.urls.full;
+  //               console.log(photo);
+  //             }
+  //           });
+  //       }
+  //     });
+  // }
 
   cars: Make[] = [
     {
